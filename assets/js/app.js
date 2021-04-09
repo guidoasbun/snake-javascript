590cdb598edf2dd6f506a7090e97bd4d7784a123
Guido Asbun <gdrodrigo8@gmail.com>
2021-04-09 00:19:48 -0700

    app.js main loop and refresh time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,27 @@
+import { SNAKE_SPEED } from './snake.js'
+
+let lastRenderTime = 0
+
+
 function main(currentTime) {
     window.requestAnimationFrame(main)
-    console.log(currentTime)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('Render')
+    lastRenderTime = currentTime
+    
+    update()
+    draw()
 }
 
+window.requestAnimationFrame(main)
+
+function update() {
+
+}
+
+function draw() {
+
+}
---
+++ assets/js/snake.js
@@ -0,0 +1,5 @@
+export const SNAKE_SPEED = 2
+
+function update() {
+    
+}
